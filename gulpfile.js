f4dfe390b9bce5fadc8b8d3ab7eb9d22892f7704
KID <kid.wumeng@gmail.com>
2018-05-13 14:42:03 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const less = require('gulp-less')
+const mini = require('gulp-minify-css')
+
+const task = gulp.task.bind(gulp)
+const dest = gulp.dest.bind(gulp)
+
+
+
+task('less', ()=>{
+   return gulp.src('themes/*.less')
+      .pipe(less())
+      .pipe(mini())
+      .pipe(dest('dist/themes'))
+})
